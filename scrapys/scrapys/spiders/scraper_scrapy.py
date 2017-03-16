420bea7a3ffa91fb20d1982ee5f2398300c13718
Christopher Kirchner <kirchnch@oregonstate.edu>
2017-03-15 23:14:52 -0700

    Added keyword to scrapy

--- scrapys/scrapys/spiders/scraper_scrapy.py
+++ scrapys/scrapys/spiders/scraper_scrapy.py
@@ -1,5 +1,6 @@
 # Script: bfs.py
-# Description: multithreaded bfs scraper that collects all links starting with single url and sends to server
+# Description: asynchronous parallel request scraper for dfs or bfs using high level library
+# (it's almost like cheating)
 # Author: Chris Kirchner
 # Email: kirchnch@oregonstate.edu
 
@@ -11,7 +12,6 @@ import json
 # modify settings in program
 # http://stackoverflow.com/questions/33094306/being-able-to-change-the-settings-while-running-scrapy-from-a-script
 
-
 class BfsScraper(scrapy.Spider):
     name = "scraper_scrapy"
 
@@ -21,19 +21,19 @@ class BfsScraper(scrapy.Spider):
         self.start_urls = [kwargs.get('start_url')]
         self.keyword = kwargs.get('keyword')
         self.link_extractor = LinkExtractor()
-    #     self.scrape_type = kwargs.get('scrape_type')
-    #     self._setup
-    #
-    # def _setup(self):
-    #     if self.cr
 
     def parse(self, response):
         # NEED TO PARSE TEXT FOR KEYWORD
         # print link to GFX
+        text = ''.join(response.selector.xpath('string()').extract())
+        keyword = False
+        if self.keyword != '' and self.keyword in text:
+            keyword = True
         link = dict(
             title=response.css('title::text').extract_first(),
             url=response.url,
-            parent_url=response.meta.get('parent', None)
+            parent_url=response.meta.get('parent', None),
+            keyword=keyword
         )
         print(json.dumps(link))
         links = self.link_extractor.extract_links(response)
--- scrapys/scrapys/spiders/scraper_wrapper.py
+++ scrapys/scrapys/spiders/scraper_wrapper.py
@@ -1,21 +1,29 @@
 # inspired by https://doc.scrapy.org/en/latest/topics/practices.html
+# Script: scraper_wrapper.py
+# Description: wrapper script for scraper_scrapy.py
+# Author: Chris Kirchner
+# Email: kirchnch@oregonstate.edu
 
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import sys, signal, os
 
+# set settings module
 os.environ['SCRAPY_SETTINGS_MODULE'] = 'scrapys.settings'
+# add scraper to path
 sys.path.append(os.path.join(os.getcwd(), 'scrapys'))
 
+# get options
 start_url = sys.argv[1]
 max_levels = sys.argv[2]
 keyword = sys.argv[3]
 search_type = sys.argv[4]
 
+
 process = CrawlerProcess(get_project_settings())
 
-
+# set scraper to follow dfs or bfs behavior
 if int(search_type) == 0:
     # DFS
     # Scrapy naturally uses DFO search order
@@ -30,12 +38,13 @@ elif int(search_type) == 1:
 
 process.settings.set('DEPTH_LIMIT', max_levels)
 
-
+# setup signal handler to catch SIGINT from mothership
 def sig_handler(sig, frame):
     process.stop()
     reactor.stop()
 
 signal.signal(signal.SIGINT, sig_handler)
 
+# start scraper
 process.crawl('scraper_scrapy', start_url=start_url, keyword=keyword)
 process.start()
